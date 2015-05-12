2db691a775726f02eb1a78b1e5c1601a36076d33
yousack <love.the.quiz.yousack@gmail.com>
2015-05-12 17:37:40 +0900

    +N-Nを追加

--- function.js
+++ function.js
@@ -2,14 +2,21 @@ function calc(status) {
     var e = document.getElementById("rule");
     var rule = e.options[e.selectedIndex].value;
 
+    var current = parseInt(document.getElementById("point").innerHTML);
+
     if (rule == 'nonx') {
         document.getElementById("point").innerHTML = document.getElementById("o").innerHTML;
     } else if (rule == 'updown') {
         if (status == 1) {
-            var current = parseInt(document.getElementById("point").innerHTML);
             document.getElementById("point").innerHTML = current + parseInt(1);
         } else {
             document.getElementById("point").innerHTML = 0;
+        }
+    } else if (rule == 'pnmn') {
+        if (status == 1) {
+            document.getElementById("point").innerHTML = current + parseInt(document.getElementById("o-point").value);
+        } else {
+            document.getElementById("point").innerHTML = current - parseInt(document.getElementById("o-point").value);
         }
     }
 }
@@ -30,4 +37,12 @@ function reset() {
     document.getElementById("point").innerHTML = 0;
     document.getElementById("o").innerHTML = 0;
     document.getElementById("x").innerHTML = 0;
+
+    var e = document.getElementById("rule");
+    var rule = e.options[e.selectedIndex].value;
+
+    if (rule == 'pnmn') {
+        document.getElementById("o-point").value = "1";
+        document.getElementById("x-point").value = "1";
+    }
 }
