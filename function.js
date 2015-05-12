5667bbafce7a4977e8295c5b0d5f5ad5130ffc1a
yousack <love.the.quiz.yousack@gmail.com>
2015-05-12 18:46:00 +0900

    +N-Nの時に不正解でも正解時の点を引いていたバグを修正

--- function.js
+++ function.js
@@ -2,34 +2,34 @@ function calc(status) {
     var e = document.getElementById("rule");
     var rule = e.options[e.selectedIndex].value;
 
-    var current = parseInt(document.getElementById("point").innerHTML);
+    var point = parseInt(document.getElementById("point").innerHTML);
 
     if (rule == 'nonx') {
         document.getElementById("point").innerHTML = document.getElementById("o").innerHTML;
     } else if (rule == 'updown') {
         if (status == 1) {
-            document.getElementById("point").innerHTML = current + parseInt(1);
+            document.getElementById("point").innerHTML = point + parseInt(1);
         } else {
             document.getElementById("point").innerHTML = 0;
         }
     } else if (rule == 'pnmn') {
         if (status == 1) {
-            document.getElementById("point").innerHTML = current + parseInt(document.getElementById("o-point").value);
+            document.getElementById("point").innerHTML = point + parseInt(document.getElementById("o-point").value);
         } else {
-            document.getElementById("point").innerHTML = current - parseInt(document.getElementById("o-point").value);
+            document.getElementById("point").innerHTML = point - parseInt(document.getElementById("x-point").value);
         }
     }
 }
 
 function o() {
-    var current = parseInt(document.getElementById("o").innerHTML);
-    document.getElementById("o").innerHTML = current + parseInt(1);
+    var oPoint = parseInt(document.getElementById("o").innerHTML);
+    document.getElementById("o").innerHTML = oPoint + parseInt(1);
     calc(1);
 }
 
 function x() {
-    var current = parseInt(document.getElementById("x").innerHTML);
-    document.getElementById("x").innerHTML = current + parseInt(1);
+    var xPoint = parseInt(document.getElementById("x").innerHTML);
+    document.getElementById("x").innerHTML = xPoint + parseInt(1);
     calc(0);
 }
 
