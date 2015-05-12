1bfd10053f4b1899865e068be567477f3e208255
yousack <love.the.quiz.yousack@gmail.com>
2015-05-12 17:10:40 +0900

    アップダウンを追加

--- function.js
+++ function.js
@@ -1,17 +1,29 @@
-function calc() {
-    document.getElementById("point").innerHTML = document.getElementById("o").innerHTML;
+function calc(status) {
+    var e = document.getElementById("rule");
+    var rule = e.options[e.selectedIndex].value;
+
+    if (rule == 'nonx') {
+        document.getElementById("point").innerHTML = document.getElementById("o").innerHTML;
+    } else if (rule == 'updown') {
+        if (status == 1) {
+            var current = parseInt(document.getElementById("point").innerHTML);
+            document.getElementById("point").innerHTML = current + parseInt(1);
+        } else {
+            document.getElementById("point").innerHTML = 0;
+        }
+    }
 }
 
 function o() {
     var current = parseInt(document.getElementById("o").innerHTML);
     document.getElementById("o").innerHTML = current + parseInt(1);
-    calc();
+    calc(1);
 }
 
 function x() {
     var current = parseInt(document.getElementById("x").innerHTML);
     document.getElementById("x").innerHTML = current + parseInt(1);
-    calc();
+    calc(0);
 }
 
 function reset() {
